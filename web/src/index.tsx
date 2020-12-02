5383a4a5e450138d7d93d00eb74040facb696644
mewhhaha <kopatheonlyone@hotmail.com>
2020-12-02 17:44:55 +0100

    Update tsconfig and add js version of aoc2

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -367,7 +367,48 @@ const Advent2Part1 = () => {
       func={(input) => {
         let numberOfPasswords: number = 0;
 
-        const js = () => {};
+        const js = () => {
+          const isValid = ({
+            min,
+            max,
+            letter,
+            password,
+          }: {
+            min: number;
+            max: number;
+            letter: string;
+            password: string;
+          }) => {
+            let sum = 0;
+            password.split("").forEach((c) => {
+              if (c === letter) sum++;
+            });
+            return sum >= min && sum <= max;
+          };
+
+          let numberOfPasswords = 0;
+          input.split(/\r?\n/).forEach((line) => {
+            const {
+              groups: { min = 0, max = 0, letter = "", password = "" } = {},
+            } =
+              /(?<min>\d+)-(?<max>\d+) (?<letter>\w): (?<password>\w+)/.exec(
+                line
+              ) ?? {};
+
+            if (!min || !max || !letter || !password) return;
+            if (
+              isValid({
+                min: Number.parseInt(min),
+                max: Number.parseInt(max),
+                letter,
+                password,
+              })
+            )
+              numberOfPasswords++;
+          });
+
+          return numberOfPasswords;
+        };
 
         const wasm = () => {
           numberOfPasswords = advent2Part1(input);
@@ -394,7 +435,46 @@ const Advent2Part2 = () => {
       func={(input) => {
         let numberOfPasswords: number = 0;
 
-        const js = () => {};
+        const js = () => {
+          const isValid = ({
+            min,
+            max,
+            letter,
+            password,
+          }: {
+            min: number;
+            max: number;
+            letter: string;
+            password: string;
+          }) => {
+            const first = password.charAt(min);
+            const second = password.charAt(max);
+            return (first === letter || second === letter) && first !== second;
+          };
+
+          let numberOfPasswords = 0;
+          input.split(/\r?\n/).forEach((line) => {
+            const {
+              groups: { min = 0, max = 0, letter = "", password = "" } = {},
+            } =
+              /(?<min>\d+)-(?<max>\d+) (?<letter>\w): (?<password>\w+)/.exec(
+                line
+              ) ?? {};
+
+            if (!min || !max || !letter || !password) return;
+            if (
+              isValid({
+                min: Number.parseInt(min),
+                max: Number.parseInt(max),
+                letter,
+                password,
+              })
+            )
+              numberOfPasswords++;
+          });
+
+          return numberOfPasswords;
+        };
 
         const wasm = () => {
           numberOfPasswords = advent2Part2(input);
